80800620bb1f5ef59de5cca4eb67849851c05f76
Sandalivihansa <114420339+Sandalivihansa@users.noreply.github.com>
2024-12-26 22:30:06 -0800

    Update config.py

--- config.py
+++ config.py
@@ -9,18 +9,17 @@ Example - JOIN_LOGGER = os.environ.get("EVENT_LOGS", "-1002325247996")
 • Some vars are already filled to help you no need to change them.
 """
 
-
 # Token from botfather 
-TOKEN = os.environ.get("7332398186:AAHG5L3MF-8BtP4ouR_9a_T2tBgje_GegN0", "7332398186:AAHG5L3MF-8BtP4ouR_9a_T2tBgje_GegN0")
+TOKEN = os.environ.get("TOKEN", "7332398186:AAHG5L3MF-8BtP4ouR_9a_T2tBgje_GegN0")
 
 # Make a new group then add @ScenarioXbot then send /id and fill id here.
-JOIN_LOGGER = os.environ.get("-1002325247996", "-1002325247996")
+JOIN_LOGGER = os.environ.get("JOIN_LOGGER", "-1002325247996")
 
 # only one # don't remove other one.
-OWNER_ID = int(os.environ.get("1451534504", "1451534504", "fill_your_id_here_")) 
+OWNER_ID = int(os.environ.get("OWNER_ID", "1451534504"))
 
 # only one # don't remove other one.
-OWNER_USERNAME = os.environ.get("OWNER_USERNAME", "CoderX", "Your_username_here")
+OWNER_USERNAME = os.environ.get("OWNER_USERNAME", "CoderX")
 
 # can add multiple with spaces
 DRAGONS = {int(x) for x in os.environ.get("DRAGONS", "").split()}
@@ -57,53 +56,50 @@ PORT = int(os.environ.get("PORT", 8443))
 CERT_PATH = os.environ.get("CERT_PATH")
 
 # Bot Owner's API_ID (From:- https://my.telegram.org/apps)
-API_ID = os.environ.get("5047271", "5047271")
+API_ID = os.environ.get("API_ID", "5047271")
 
 # Bot Owner's API_HASH (From:- https://my.telegram.org/apps)
-API_HASH = os.environ.get("047d9ed308172e637d4265e1d9ef0c27", "047d9ed308172e637d4265e1d9ef0c27")
+API_HASH = os.environ.get("API_HASH", "047d9ed308172e637d4265e1d9ef0c27")
 
 # Any SQL Database Link (RECOMMENDED:- PostgreSQL & https://www.elephantsql.com)
 DB_URL = os.environ.get("DATABASE_URL", "") 
 
 # Don't touch
-DB_URL = DB_URL.replace(
-"postgres://", "postgresql://", 1
-)  # rest of connection code using the connection string `uri`
+DB_URL = DB_URL.replace("postgres://", "postgresql://", 1)  # rest of connection code using the connection string `uri`
 
 # Donation Link (ANY)
-DONATION_LINK = os.environ.get("https://t.me/i_14344")
+DONATION_LINK = os.environ.get("DONATION_LINK", "https://t.me/i_14344")
 
 # Wall api key for wallpapers # From:- https://wall.alphacoders.com/api.php
-WALL_API = os.environ.get("WALL_API", None) 
+WALL_API = os.environ.get("WALL_API", None)
 
 # To remove background of images  # From: https://www.remove.bg/
-REM_BG_API_KEY = os.environ.get("qV26c84dUAhUuEb3sG9N9M9Q", "qV26c84dUAhUuEb3sG9N9M9Q")
+REM_BG_API_KEY = os.environ.get("REM_BG_API_KEY", "qV26c84dUAhUuEb3sG9N9M9Q")
 
-## More info written at right side from this line.
-
-OPENWEATHERMAP_ID = os.environ.get("OPENWEATHERMAP_ID", "") # From:- https://openweathermap.org/api
-GENIUS_API_TOKEN = os.environ.get("GENIUS_API_TOKEN", None) # From:- http://genius.com/api-clients
+# More info written at right side from this line.
+OPENWEATHERMAP_ID = os.environ.get("OPENWEATHERMAP_ID", "")  # From:- https://openweathermap.org/api
+GENIUS_API_TOKEN = os.environ.get("GENIUS_API_TOKEN", None)  # From:- http://genius.com/api-clients
 MONGO_DB_URL = os.environ.get("MONGO_DB_URL", "mongodb+srv://Cluster006:600510@cluster006.ootpa.mongodb.net/Cluster006?retryWrites=true&w=majority")
 REDIS_URL = os.environ.get("REDIS_URL", "redis://Madharjoot:GuKhao123_@redis-12276.c275.us-east-1-4.ec2.cloud.redislabs.com:12276/Madharjoot")
-BOT_ID = int(os.environ.get("BOT_ID", None)) # Telegram Bot ID (EXP:- 1241223850)
-SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", None) # Support Chat Group Link (Use @ScenarioXSupport || Dont Use https://t.me/ScenarioXSupport)
-SPAMWATCH_SUPPORT_CHAT = os.environ.get("SPAMWATCH_SUPPORT_CHAT", None) # Use @SpamWatchSupport
-SPAMWATCH_API = os.environ.get("SPAMWATCH_API", None) # From https://t.me/SpamWatchBot 
-BOT_USERNAME = os.environ.get("BOT_USERNAME", "") # Bot Username
+BOT_ID = int(os.environ.get("BOT_ID", "None"))  # Telegram Bot ID (EXP:- 1241223850)
+SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", None)  # Support Chat Group Link
+SPAMWATCH_SUPPORT_CHAT = os.environ.get("SPAMWATCH_SUPPORT_CHAT", None)  # Use @SpamWatchSupport
+SPAMWATCH_API = os.environ.get("SPAMWATCH_API", None)  # From https://t.me/SpamWatchBot 
+BOT_USERNAME = os.environ.get("BOT_USERNAME", "")  # Bot Username
 
-STRING_SESSION = os.environ.get("STRING_SESSION", None) # Telethon Based String Session (2nd ID) [ From https://repl.it/@SpEcHiDe/GenerateStringSession ]
+STRING_SESSION = os.environ.get("STRING_SESSION", None)  # Telethon Based String Session
 REPO = "TeamScenario/Scenario"
 DEVELOPER = "TeamScenario"
 APP_ID = API_ID
 APP_HASH = API_HASH
-HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME", True) # Heroku App Name 
-HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", True) # Heroku API [From https://dashboard.heroku.com/account]
+HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME", True)  # Heroku App Name
+HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", True)  # Heroku API Key
 UPSTREAM_BRANCH = os.environ.get("UPSTREAM_BRANCH", True)
 UPSTREAM_REPO = os.environ.get("UPSTREAM_REPO", True)
 YOUTUBE_API_KEY = os.environ.get("YOUTUBE_API_KEY", True)
-ALLOW_CHATS = os.environ.get("ALLOW_CHATS", True) # Don't Change
-BOT_NAME = os.environ.get("BOT_NAME", True) # Name Of your Bot.
-MONGO_DB = "scenario" # Don't change else errors.
+ALLOW_CHATS = os.environ.get("ALLOW_CHATS", True)  # Don't Change
+BOT_NAME = os.environ.get("BOT_NAME", True)  # Name Of your Bot.
+MONGO_DB = "scenario"  # Don't change else errors.
 ARQ_API_URL = "https://arq.hamker.in"
 GOOGLE_CHROME_BIN = "/usr/bin/google-chrome"
 CHROME_DRIVER = "/usr/bin/chromedriver"
@@ -118,9 +114,8 @@ scenario_pic = os.environ.get("scenario_pic", True) or "https://telegra.ph/file/
 
 BL_CHATS = {int(x) for x in os.environ.get("BL_CHATS", "").split()}
 
-
 # Don't Change
-LOAD = os.environ.get("LOAD", "").split() 
+LOAD = os.environ.get("LOAD", "").split()
 
 # Don't Change
 NO_LOAD = os.environ.get("NO_LOAD", "translation").split()
